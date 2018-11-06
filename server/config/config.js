38b42ec72b3fa9e29cb892e274ef4aa205d34ae3
CRLaughlin <chrisgator11@live.com>
2018-11-06 09:11:25 -0500

    config correction

--- server/config/config.js
+++ server/config/config.js
@@ -3,11 +3,11 @@ module.exports = {
 		uri: 'mongodb://CENTeam3:CENTeam3@ds141633.mlab.com:41633/games_info', //place the URI of your mongo database here.
 	},
 	db2: {
-		uri: 'mongodb://CENTeam3:CENTeam3@ds141633.mlab.com:41633/events_info', //place the URI of your mongo database here.
+		uri: 'mongodb://CENTeam3:CENTeam3@ds141613.mlab.com:41613/events_info', //place the URI of your mongo database here.
 
 	},
 	db3: {
-		uri: 'mongodb://CENTeam3:CENTeam3@ds141633.mlab.com:41633/accounts_info', //place the URI of your mongo database here.
+		uri: 'mongodb://CENTeam3:CENTeam3@ds141633.mlab.com:41633/account_info', //place the URI of your mongo database here.
 
 	},
 	port : 8080,
