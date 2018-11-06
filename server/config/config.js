bab175dbd28c351bb2d2afad77883f8417cc4457
CRLaughlin <chrisgator11@live.com>
2018-11-06 01:10:25 -0500

    host page

--- server/config/config.js
+++ server/config/config.js
@@ -2,5 +2,13 @@ module.exports = {
 	db: {
 		uri: 'mongodb://CENTeam3:CENTeam3@ds141633.mlab.com:41633/games_info', //place the URI of your mongo database here.
 	},
+	db2: {
+		uri: 'mongodb://CENTeam3:CENTeam3@ds141633.mlab.com:41633/events_info', //place the URI of your mongo database here.
+
+	},
+	db3: {
+		uri: 'mongodb://CENTeam3:CENTeam3@ds141633.mlab.com:41633/accounts_info', //place the URI of your mongo database here.
+
+	},
 	port : 8080,
 };
