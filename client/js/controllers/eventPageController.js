31ea6a8c1a801d4b3727e3ffc49ca256226624e9
malkiewiczm <malkiewiczm@users.noreply.github.com>
2018-11-28 23:46:59 -0500

    google maps integrated
    now uses's the event's location

--- client/js/controllers/eventPageController.js
+++ client/js/controllers/eventPageController.js
@@ -44,6 +44,7 @@ window.addEventListener('load', function() {
 			event : evt_id
 		}));
 	};
+	
 	if (window.location.hash) {
 		var evt_id = window.location.hash.substr(1);
 		get_event(evt_id, function(obj) {
@@ -53,6 +54,7 @@ window.addEventListener('load', function() {
 			set_text('f_time', obj.time);
 			set_text('f_location', obj.location);
 			set_text('f_games', obj.gamesavailable);
+			change_addr(obj.location);
 		}, function(err) {
 			set_text('f_name', err);
 		});
@@ -66,4 +68,30 @@ window.addEventListener('load', function() {
 	} else {
 		set_text('f_name', 'No game specified');
 	}
+	
+	function change_addr(address) {
+      	var geocoder = new google.maps.Geocoder();
+
+		geocoder.geocode( { 'address': address}, function(results, status) {
+
+  			if (status == google.maps.GeocoderStatus.OK) {
+    			var latitude = results[0].geometry.location.lat();
+    			var longitude = results[0].geometry.location.lng();
+
+    			var myLatLng = {lat: latitude, lng: longitude};
+
+        		var map = new google.maps.Map(document.getElementById('map'), {
+         			zoom: 16,
+         			center: myLatLng
+       			});
+
+        		var marker = new google.maps.Marker({
+          			position: myLatLng,
+          			map: map,
+         			title: address
+        		});
+ 			}
+		});
+
+    }
 });
