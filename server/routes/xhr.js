9103c6e640a8591f37fdc21e415c38fa135d3184
zhouhao0218 <zhouhao0218@ufl.edu>
2018-12-05 00:31:09 -0500

    created searchController and backend routes file but does not work

---
+++ server/routes/xhr.js
@@ -0,0 +1,32 @@
+var gameEvents = require('../models/events.server.model.js');
+var express = require('express');
+var router = express.Router();
+
+router.get('/search', function(req, res, next){
+  var q = req.query.q;
+
+  // gameEvents.find({
+  //     $text: {
+  //         $search: q
+  //     }
+  // }, {
+  //   _id: 0,
+  //   __v: 0
+  // }, function(err,data){
+  //   res.json(data);
+  // });
+
+  gameEvents.find({
+    name: {
+      $regex: new RegExp(q)
+    }
+  }, {
+    _id: 0,
+    __v: 0
+  }, function(err, data){
+    res.json(data);
+  }).limit(20);
+
+});
+
+module.exports = router;
